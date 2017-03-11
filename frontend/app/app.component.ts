487cf748a7995aeac77c411510d8b9d4648d3285
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-03-11 10:03:01 -0500

    Remove Unimplemented Feature Links for now

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -13,11 +13,11 @@ import { Component } from '@angular/core';
           <ul class="nav navbar-nav">
             <li class="nav-item" routerLinkActive="active"><a class="nav-link" routerLink="/proportions"><i class="fa fa-tint" aria-hidden="true"></i>&nbsp;Products</a></li>
             <li class="nav-item" routerLinkActive="active"><a class="nav-link" routerLink="/geos"><i class="fa fa-globe" aria-hidden="true"></i>&nbsp;Partners</a></li>
-            <li class="nav-item"><a class="nav-link"><i class="fa fa-line-chart" aria-hidden="true"></i>&nbsp;Trends</a></li>
-            <li class="nav-item"><a class="nav-link"><i class="fa fa-object-ungroup" aria-hidden="true"></i>&nbsp;Compare</a></li>
+            <!-- <li class="nav-item"><a class="nav-link"><i class="fa fa-line-chart" aria-hidden="true"></i>&nbsp;Trends</a></li>
+            <li class="nav-item"><a class="nav-link"><i class="fa fa-object-ungroup" aria-hidden="true"></i>&nbsp;Compare</a></li> -->
           </ul>
           <ul class="nav navbar-nav float-xs-right">
-            <li class="nav-item"><a class="nav-link"><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp;Analysis</a></li>
+            <!-- <li class="nav-item"><a class="nav-link"><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp;Analysis</a></li> -->
             <li class="nav-item" routerLinkActive="active"><a href="#" class="nav-link" routerLink="/about"><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;About</a></li>
           </ul>
         <!-- </div>--><!-- /.navbar-collapse -->
